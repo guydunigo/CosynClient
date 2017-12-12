b185d814296da23f1892bf9ffb75287b590f06bf
GONI Guillaume <gdn3@live.fr>
2017-12-12 11:27:45 +0100

    mock-kbs

---
+++ src/app/mock-kbs.ts
@@ -0,0 +1,87 @@
+import { Key } from './key';
+import { Keyboard } from './keyboard';
+
+export const KBS = [
+    {
+        id: 0,
+        name: 'piano',
+        keys: [
+            {
+                id: 0,
+                name: 'do',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 1,
+                name: 're',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 2,
+                name: 'mi',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 3,
+                name: 'fa',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 4,
+                name: 'sol',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'clavecin',
+        keys: [
+            {
+                id: 0,
+                name: 'do',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 1,
+                name: 're',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 2,
+                name: 'mi',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 3,
+                name: 'fa',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            },
+            {
+                id: 4,
+                name: 'sol',
+                src: '/musics/',
+                enabled: false,
+                volume: 1.
+            }
+        ]
+    }
+];
